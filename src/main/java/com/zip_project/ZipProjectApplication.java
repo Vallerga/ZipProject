7a8e457d1906ebc210cf8c87bc6a4d0c224b8290
Vallerga Antonio <96259359+Vallerga@users.noreply.github.com>
2024-05-23 16:11:06 +0200

    Spring project created

---
+++ src/main/java/com/zip_project/ZipProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zip_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zip_project/ZipProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zip_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
